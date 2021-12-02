725f4ca505fd95061e194d69cb7ee2736bfff213
asmith <adam.smith@aquilaheywood.co.uk>
2021-12-02 09:09:30 +0000

    add headers

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem()
+{
+    return (<div>
+                <div>01.12.2021</div>
+                <div>
+                    <h2>Car Insurance</h2>
+                    <div>385.24</div>
+                </div>
+            </div>
+    );
+}
+
+export default ExpenseItem;
