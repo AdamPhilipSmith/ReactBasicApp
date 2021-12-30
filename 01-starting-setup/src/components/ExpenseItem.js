e3246f74acb3cace4fc5de14ad8a5be16813aa02
asmith <adam.smith@aquilaheywood.co.uk>
2021-12-30 15:58:06 +0000

    Assignment1 complete

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
-function ExpenseItem()
+import ExpenseDate from "./ExpenseDate";
+import './ExpenseItem.css'
+
+function ExpenseItem(props)
 {
-    return (<div>
-                <div>01.12.2021</div>
-                <div>
-                    <h2>Car Insurance</h2>
-                    <div>385.24</div>
+    return (
+            <div className="expense-item">
+                <ExpenseDate date={props.date}/>
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">£{props.amount}</div>
                 </div>
             </div>
     );
